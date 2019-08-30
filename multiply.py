595f86b0b22fa003a0dea29e21b792d529c5d3b3
ddutta98 <32028770+ddutta98@users.noreply.github.com>
2019-08-30 10:42:44 +0530

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -1,4 +1,5 @@
-    first_number = int(input("Enter your first number : "))
-    second_number = int(input("Enter your second number : "))
-    final_result = first_number * second_number
-    print("Multiplication of {} and {} is : {}".format(first_number,second_number,final_result))
+    def mul():
+    
+            first_number = int(input("Enter your first number : "))
+            second_number = int(input("Enter your second number : "))
+            return first_number * second_number
