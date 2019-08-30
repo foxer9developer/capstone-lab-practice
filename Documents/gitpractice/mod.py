853294cc4f8c30d85583e1593bdc2edf1147a2f3
Vaibhav Goel <goelvaibhav379@gmail.com>
2019-08-30 15:48:13 +0530

    modulo

--- Documents/gitpractice/mod.py
+++ Documents/gitpractice/mod.py
@@ -1,1 +1,8 @@
-print("modulo chosen")
+def mod():
+	print("modulo chosen")
+	a = int(input("enter A: ")
+	b = int(input("enter B: ")
+	if b == 0:
+		print("B should not be 0")
+	else:
+		print(a%b)
