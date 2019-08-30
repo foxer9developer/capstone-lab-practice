42082cb444ddf632cb1c18e7a62f279d2714f837
Vaibhav Goel <goelvaibhav379@gmail.com>
2019-08-30 15:33:13 +0530

    first commit

--- Documents/gitpractice/options.py
+++ Documents/gitpractice/options.py
@@ -2,6 +2,21 @@ print("press 1 for add")
 print("press 2 for subtract")
 print("press 3 for multiply")
 print("press 4 for divide")
+print("press 5 for modulo")
+
+
+option = int(input("option: "))
+
+if option == 1:
+	add.add()
+elif option == 2:
+	sub.sub()
+elif option == 3:
+	mul.mul()
+elif option == 4:
+	div.div()
+elif option == 5:
+	mod.mod()
 
 print(" enter a")
 print(" enter b")
