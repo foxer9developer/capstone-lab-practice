0ba75e1f3e101fe6b42a5a0d8aeac8e42c29ac30
Vaibhav Goel <goelvaibhav379@gmail.com>
2019-08-30 15:15:17 +0530

    first commit again

--- Documents/gitpractice/options.py
+++ Documents/gitpractice/options.py
@@ -1,5 +1,8 @@
 print("press 1 for add")
-print("press 1 for subtract")
-print("press 1 for multiply")
-print("press 1 for divide")
+print("press 2 for subtract")
+print("press 3 for multiply")
+print("press 4 for divide")
+
+print(" enter a")
+print(" enter b")
 
