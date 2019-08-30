0e65eb4563d80e3113a3693e86c3588558f254c0
ddutta98 <32028770+ddutta98@users.noreply.github.com>
2019-08-30 11:04:09 +0530

    Update and rename multiply.py to mul.py

--- mul.py
+++ mul.py
@@ -2,4 +2,4 @@
     
             first_number = int(input("Enter your first number : "))
             second_number = int(input("Enter your second number : "))
-            return first_number * second_number
+            print( first_number * second_number)
