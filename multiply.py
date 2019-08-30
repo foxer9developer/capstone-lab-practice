0bd04e04ac98101e729337e103d73108cf25636e
shreyansh9991 <shreyanshdubey669@gmail.com>
2019-08-30 21:12:34 +0530

    multiply

---
+++ multiply.py
@@ -0,0 +1,4 @@
+    first_number = int(input("Enter your first number : "))
+    second_number = int(input("Enter your second number : "))
+    final_result = first_number * second_number
+    print("Multiplication of {} and {} is : {}".format(first_number,second_number,final_result))
